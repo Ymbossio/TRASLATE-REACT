281ed4c95b1fdce8eb90550bc2534ba08799f70c
Yovanis Bossio <129323785+Ymbossio@users.noreply.github.com>
2024-05-08 15:12:51 +0000

    inicializacion del proyecto y css

---
+++ TRADUCTOR-TEXTO/src/Result.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+
+
+const Result = () =>{
+    return(
+       <ul>
+        <li className='world'>
+            <h2>Hello</h2>
+            <span>/hhdhs/ hru </span>
+        </li>
+
+        <li className='container'>
+            <h3>noun</h3>
+            <div className='details meaning'>
+                <h3>Meaning</h3>
+                <p>Hello, or na equivaleny greeting</p>
+            </div>
+
+            <div className='details synonyms'>
+                <h3>synonyms</h3>
+                <span>greeting, greeting, greeting  </span>
+                <span>greeting, greeting, greeting  </span>
+                <span>greeting, greeting, greeting  </span>
+            </div>
+
+        </li>
+       </ul>
+    )
+}
+
+export default Result;
